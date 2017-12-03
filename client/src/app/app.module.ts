1ecbaeadce6e6a4a914c31414c75e0447721e66a
gemaderus <hi@gemaderus.com>
2017-12-03 13:16:19 +0100

    Added header and home component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
